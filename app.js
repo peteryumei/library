0a7370a194107b373aa772db3e5f604cb4788b33
Peter Mei <peter.mei@autoalert.com>
2019-03-13 14:35:23 -0700

    Add Pug

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index', { list: ['a', 'b'] });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   debug(`listening on port + ${chalk.green(port)}`);
 });
