b5e84aa3058e0549dc68eabe7548d24a15ae0a81
Peter Mei <peter.mei@autoalert.com>
2019-03-15 15:55:21 -0700

    add sql database script

---
+++ database.sql
@@ -0,0 +1,15 @@
+create table books (id int not null,
+				title varchar(255),
+				author varchar(255))
+
+go
+
+insert into books (id, title, author) values
+(1,  'War and Peace', 'lev Nikolayevich Tolstoy'),
+(2,  'Les Miserables', 'Victor Hugo'),
+(3, 'The Time Machine', 'H. G. Wells'),
+(4, 'A Journey into the Center of the Earth', 'Jules Verne'),
+(5,  'The Dark World', 'Henry Kuttner'),
+(6, 'The Wind in the Willows', 'Kenneth Grahame'),
+(7, 'Life On The Mississippi', 'Mark Twain'),
+(8, 'Childhood', 'Lev Nikolayevich Tolstoy')
